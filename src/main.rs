51a62660bbb15289d1e4b97f2e3bfb798fd16606
Bryan Matheus <bryan.matheusbmf@gmail.com>
2022-04-03 20:00:12 -0300

    fix: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, fs, process, error::Error};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,10 +11,18 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file.");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("With text:\n {}", contents);
+    
+    Ok(())
 }
 
 struct Config {
