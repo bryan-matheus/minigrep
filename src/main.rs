8abba46d0b614aff3a5762069abd5757da1626d6
Bryan Matheus <bryan.matheusbmf@gmail.com>
2022-04-03 19:53:26 -0300

    fix: improve error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -20,10 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments.");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Config { query, filename }
+       Ok(Config { query, filename })
     }
 }
